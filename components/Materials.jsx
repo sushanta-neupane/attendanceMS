c2d459efdc7e491e314e54d0f32d3028265e4525
Sushanta Neupane <sushantaneupane123@gmail.com>
2023-07-17 19:38:53 +0545

    testing

--- components/Materials.jsx
+++ components/Materials.jsx
@@ -1,42 +1,12 @@
 "use client"
-import Link from "next/link";
-import React, {useState, useEffect} from "react";
+import React from 'react'
 
 function Materials() {
-  const path = window.location.pathname; 
-  const folderid = '1QceEYxfhjBrlFRoIYjG2s0ogdDX9G3Ka';
-
-  const [filesList, setFiles] = useState([]);
-
-  const fetchData = async () => {
-    const {drive} = await (await fetch(`/api/folderfetch/?id=${folderid}`)).json(); 
-    const {files} = drive;
-    setFiles(files);
-  }
-
-  const handleClick = (e) =>{
-console.log(e.target.id)
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
-    <div className="main-container materials">
-      {filesList.map(item => (
-        <Link href={`${path}/${item.name.replace(/\s/g, '').toLowerCase()}`}>
-        <div key={item.id} className="folder">
-         
-          <div id={item.id} onClick={ handleClick } className="subfolder">
-            {item.name}     
-          </div>
-     
-        </div>
-        </Link>
-      ))}
+    <div className="main-container">
+      materials
     </div>
-  );
+  )
 }
 
-export default Materials;
+export default Materials
